43957d4df8c8ec5413a880b7c330c2b58e4b39a4
mwborn <33558897+mwborn@users.noreply.github.com>
2025-12-28 00:47:43 +0100

    feat: Use CDN for Firebase dependencies
    Update Vite configuration to use CDN imports for Firebase compat modules. This aligns with the `index.html` setup and ensures consistent dependency loading.

--- vite.config.js
+++ vite.config.js
@@ -6,11 +6,20 @@ export default {
     // dove il comando xdg-open non è disponibile.
     open: false,
   },
-  optimizeDeps: {
-    exclude: [
-      'firebase/compat/app',
-      'firebase/compat/auth',
-      'firebase/compat/firestore'
+  resolve: {
+    alias: [
+      { 
+        find: 'firebase/compat/app', 
+        replacement: 'https://aistudiocdn.com/firebase@^10.12.2/compat/app' 
+      },
+      { 
+        find: 'firebase/compat/auth', 
+        replacement: 'https://aistudiocdn.com/firebase@^10.12.2/compat/auth' 
+      },
+      { 
+        find: 'firebase/compat/firestore', 
+        replacement: 'https://aistudiocdn.com/firebase@^10.12.2/compat/firestore' 
+      },
     ]
   }
 };
